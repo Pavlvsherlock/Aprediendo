65b5d7bd8521f40ef20e664b96848255e86b0481
Pavlvsherlock <p.abalo.perez@gmail.com>
2024-12-07 18:47:04 +0100

    primer commit

---
+++ proyectocsharp/proyectouno/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kali {
+    class Programa {
+        static void Main(string[] args) {
+            Console.WriteLine("Hola desde kali");
+	    Console.WriteLine("Hola desde a consola");
+	    List<int> miLista = new List<int>();
+	    miLista.Add(8);
+	    miLista.Add(12);
+	    miLista.Add(4);
+	    foreach (int nu in miLista){
+		    Console.Write(nu +" ");
+	    }
+        miLista.Sort();
+        Console.WriteLine("");
+        foreach (int num in miLista){
+            Console.Write(num +" ");
+        }
+        int total=0;
+        Console.WriteLine("");
+        foreach (int numero in miLista){
+            total += numero;
+        }
+        Console.WriteLine("suma total: " +total);
+        
+        string nombre=Console.ReadLine();
+        Console.WriteLine("Hola {0}",nombre);
+        Console.WriteLine("adios");
+        
+
+    }
+    }
+}
